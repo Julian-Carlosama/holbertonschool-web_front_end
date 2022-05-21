d236126bc517b1250d10456ca356904c5f88d496
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-05-20 23:58:14 -0500

    13. Binding + Closure

--- 0x0C-Javascript_advanced/13-bind_user.js
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -1,1 +1,17 @@
-//
+// Object user with the attributes:
+let user = {
+       hobby: 'Calligraphy',
+       favoriteSport: 'Hockey',
+       astrologicalSign: 'Aries',
+       firstName: 'Buillaume',
+       lastName: 'Ialva',
+       location: 'Telaviv',
+       occupation: 'Engineer',
+     
+       logWelcomeUser(welcomeString) {
+         console.log(`${welcomeString}, ${this.firstName} Your occupation is: ${this.occupation}`);
+       }
+     };
+     
+     let bindLogWelcomeUser = user.logWelcomeUser.bind(user);
+     bindLogWelcomeUser('Hello');
